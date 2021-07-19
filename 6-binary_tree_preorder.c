90e91ca1ad36f1e9917440cd04c5dd676b1d4792
toshi-uy <s_borgia@hotmail.com>
2021-07-19 16:14:19 -0300

    changed logic

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,32 +6,11 @@
  * @func: value of new node
  */
 
-void recursion_left(const binary_tree_t *aux, void (*func)(int))
-{
-	func(aux->n);
-	if(!aux->left)
-	{
-		aux = aux->left;
-	}
-	recursion_left(aux, func);
-}
-void recursion_right(const binary_tree_t *aux, void (*func)(int))
-{
-	func(aux->n);
-	if (aux->right)
-	{
-		aux = aux->right;
-	}
-	recursion_right(aux, func);
-}
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	const binary_tree_t *aux = NULL;
-
 	if (!tree || !func)
 		return;
-	aux = tree;
-
-	recursion_left(aux, func);
-	recursion_right(aux, func);
+	printf("%d\n", tree->n);
+	binary_tree_preorder(tree, func);
+	binary_tree_preorder(tree, func);
 }
