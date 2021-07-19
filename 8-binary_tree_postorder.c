135d050076006ff17efd75eeb2d9320416b17693
toshi-uy <s_borgia@hotmail.com>
2021-07-19 16:20:35 -0300

    added 8 and removed aux protoypes

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Entry point
+ * @tree: pointer to tree root
+ * @func: value of new node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	printf("%d\n", tree->n);
+}
