0ef2a82dc216c0defc6bf4b0f5164226af47bd09
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-07-19 17:56:53 +0000

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: pointer to node to check
+ * Return: 1 if leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->right && !node->left)
+		return (1);
+	return (0);
+}
