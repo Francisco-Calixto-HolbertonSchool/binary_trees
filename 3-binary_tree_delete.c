fbad70db64154cf4b57f6b1ffa120c35b5dafc95
toshi-uy <s_borgia@hotmail.com>
2021-07-19 16:53:25 -0300

    testing 3 recursive

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * recursive_free - Entry point
+ * @aux: pointer to tree root
+ */
+recursive_free(binary_tree_t *aux)
+{
+	if (!aux)
+		return;
+	recursive_free(aux->left);
+	recursive_free(aux->right);
+	free(aux);
+}
+/**
+ * binary_tree_postorder - Entry point
+ * @tree: pointer to tree root
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *aux;
+
+	if (!tree)
+		return;
+	aux = tree;
+	recursive_free(aux);
+	free(tree);
+}
