67ea181eb37432c051cf540eccd403957a2a8e6a
toshi-uy <s_borgia@hotmail.com>
2021-07-20 19:50:48 -0300

    testing kiko theory

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,11 +13,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		leaves += 1;
-	else
 	{
 		binary_tree_leaves(tree->right);
 		binary_tree_leaves(tree->left);
 	}
+	else
+		leaves += 1;
 	return (leaves);
 }
