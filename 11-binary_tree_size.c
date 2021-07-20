84a2c202edcd286999f6d695b163718fcb0a0bf0
toshi-uy <s_borgia@hotmail.com>
2021-07-20 19:17:04 -0300

    testing size inside if

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,11 +13,17 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		size += (1 + binary_tree_size(tree->left));
+	{
+		binary_tree_size(tree->left);
+		size++;
+	}
 	else
-		size += 1;
+		size += 0;
 	if (tree->right)
-		size += (1 + binary_tree_size(tree->right));
+	{
+		binary_tree_size(tree->right);
+		size++;
+	}
 	else
 		size += 0;
 	return (size);
