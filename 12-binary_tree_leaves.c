c10953f1bba30dc1b273c29c4aae942c3bda2c56
toshi-uy <s_borgia@hotmail.com>
2021-07-20 18:55:05 -0300

    added task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Entry point
+ * @tree: pointer to tree root
+ * Return: size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	binary_tree_size(tree->left);
+	if (!tree->right && !tree->left)
+		size++;
+	binary_tree_size(tree->right);
+}
