5ab5ae22c2b8a0653ea79facfa70f5b3c08ef8d6
toshi-uy <s_borgia@hotmail.com>
2021-07-20 18:43:30 -0300

    fixed recursion

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	binary_tree_inorder(tree->left);
+	binary_tree_size(tree->left);
 	size++;
-	binary_tree_inorder(tree->right);
+	binary_tree_size(tree->right);
 }
