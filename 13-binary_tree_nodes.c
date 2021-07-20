57adfff6a913dc5ba59e3f2abdc4efd2ff4b5424
toshi-uy <s_borgia@hotmail.com>
2021-07-20 20:17:59 -0300

    adding 13 task

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes in a binary tree
+ * @tree: pointer to tree root
+ * Return: number of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		nodes++;
+	nodes = binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
+	return (nodes);
+}
+
