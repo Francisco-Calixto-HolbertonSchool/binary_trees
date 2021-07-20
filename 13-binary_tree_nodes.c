7d456e893d6fa8fab2c28d19bca4bafcec53d443
toshi-uy <s_borgia@hotmail.com>
2021-07-20 20:44:12 -0300

    cleaning code

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -44,14 +44,4 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
-	/*
-	size_t nodes = 0;
-
-	if (!tree)
-		return (0);
-	if (tree->left || tree->right)
-		return (1);
-	nodes = binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
-	return (nodes);
-	*/
 }
