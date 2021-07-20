9f7d478938817799a94726c85b9c71e91113c199
toshi-uy <s_borgia@hotmail.com>
2021-07-20 20:22:24 -0300

    testing +=

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-	{
-		nodes++;
-		return (nodes);
-	}
+		return (1);
 	nodes += binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
 	return (nodes);
 }
