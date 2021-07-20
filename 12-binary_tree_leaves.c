88ed5dac82945ef4d2968a9ce7bb7d6ce53653ac
toshi-uy <s_borgia@hotmail.com>
2021-07-20 20:06:12 -0300

    testing returns

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,12 +12,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->left || !tree->right)
-	{
-		binary_tree_leaves(tree->right);
-		binary_tree_leaves(tree->left);
-	}
-	else
-		leaves += 1;
-	return (leaves + 1);
+	if (!tree->left && !tree->right)
+		return (1);
+	leaves = binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left);
+	return (leaves);
 }
+
