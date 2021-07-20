e0df67b396ce3269b41b1d9482f6e09d09b9a19c
toshi-uy <s_borgia@hotmail.com>
2021-07-20 20:41:28 -0300

    testing kiko is genious

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,41 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - Entry point
+ * @tree: pointer to tree root
+ * Return: size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (!tree)
+		return (0);
+	size = (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (size);
+}
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to tree root
+ * Return: number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	leaves = binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left);
+	return (leaves);
+}
+
+
+/**
  * binary_tree_nodes - counts the nodes in a binary tree
  * @tree: pointer to tree root
  * Return: number of nodes
@@ -8,13 +43,15 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+	/*
 	size_t nodes = 0;
 
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
 		return (1);
-	nodes = binary_tree_nodes(tree->right) + 1 + binary_tree_nodes(tree->left);
+	nodes = binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left);
 	return (nodes);
+	*/
 }
-
