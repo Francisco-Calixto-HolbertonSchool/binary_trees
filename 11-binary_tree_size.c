91f2c037b2d4b6800ddea4548214702ed224cfa8
toshi-uy <s_borgia@hotmail.com>
2021-07-20 16:46:42 -0300

    removed elses

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,13 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		left = (1 + binary_tree_size(tree->left));
-	else
-		left += 0;
-	if (tree->right)
-		right = (1 + binary_tree_size(tree->right));
-	else
-		right += 0;
+	left = (1 + binary_tree_size(tree->left));
+	right = (1 + binary_tree_size(tree->right));
 	return (right + left);
 }
