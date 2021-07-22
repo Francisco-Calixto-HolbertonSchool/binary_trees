347a87bd10b4a228b7eddf1c940e719da0b51ae8
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-07-22 17:09:18 +0000

    task 100

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+#include "10-binary_tree_depth.c"
+
+/**
+ * binary_tree_depth - Entry point
+ * @tree: pointer to node of tree to measure the depth
+ * Return: depth of tree
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	const binary_tree_t *root;
+	size_t d_1, d_2;
+
+	if (!first->parent || !second->parent)
+		return (NULL);
+	root = first;
+	d_1 = binary_tree_depth(first);
+	d_2 = binary_tree_depth(second);
+	while (root->parent)
+		root = root->parent;
+	while (d_1 < d_2)
+	{
+		first = first->parent;
+		d_1--;
+	}
+	while (d_1 > d_2)
+	{
+		second = second->parent;
+		d_2--;
+	}
+	if (first->parent == second->parent)
+		return (first->parent);
+	return ((binary_tree_t *) root);
+}
