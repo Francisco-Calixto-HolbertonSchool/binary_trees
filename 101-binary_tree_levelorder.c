13e2f9d3a7f715169a4b3022ea49fbdf5abc0d2e
toshi-uy <s_borgia@hotmail.com>
2021-07-22 21:15:10 -0300

    added aux function

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+#include "10-binary_tree_depth.c"
+#include "9-binary_tree_height.c"
+
+/**
+ * aux - Entry point
+ * @tree: pointer to root
+ * @i: iterator
+ * @func: function
+ */
+
+void aux(binary_tree_t *tree, int i, void (*func)(int))
+{
+	if(binary_tree_depth(tree) == i)
+		func(tree->n);
+	aux(tree->left, i, func);
+	aux(tree->right, i, func);
+}
+
+/**
+ * binary_tree_levelorder - Entry point
+ * @tree: pointer to root
+ * @func: pointer to func
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t depth, i, height;
+
+	if (!tree || !func)
+		return (NULL);
+
+	depth = binary_tree_depth(tree);
+	height = binary_tree_height(tree);
+	if (depth == 0)
+		func(tree->n);
+	for (i = 1; i <= height; i++)
+	{
+		aux(tree, i, func);
+	}
+}
