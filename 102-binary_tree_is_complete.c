49321dfa629e4d87aba805c3a401d0963149d03c
toshi-uy <s_borgia@hotmail.com>
2021-07-22 22:05:19 -0300

    testing

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -19,7 +19,7 @@ int binary_tree_is_perfect_1(const binary_tree_t *tree)
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	if (!tree || binary_tree_is_perfect_1(tree) == 0)
+	if (!tree)
 		return (0);
 	if (binary_tree_is_perfect(tree) == 1)
 		return (1);
